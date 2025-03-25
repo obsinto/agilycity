28979b97cbf16aa2686309f2e8ae7dfdaa0bc59a
Deyvid Sant <deyvidsant@hotmail.com>
2025-03-25 14:23:52 -0300

    ajustes https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
+
         Carbon::setLocale(config('app.locale'));
         $this->app['router']->aliasMiddleware('role', RoleMiddleware::class);
     }
